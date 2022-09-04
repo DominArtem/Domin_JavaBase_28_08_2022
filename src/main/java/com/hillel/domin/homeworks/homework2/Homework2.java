9b5dbc2103abaaca81aa45b6f9b6ced23689560f
Domin Artem <itomas612@gmail.com>
2022-09-04 22:13:31 +0300

    added source code

---
+++ src/main/java/com/hillel/domin/homeworks/homework2/Homework2.java
@@ -0,0 +1,12 @@
+package com.hillel.domin.homeworks.homework2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        double doubleVariable = -14.738845423422779;
+        double doubleVariable1 = -75.1292754392072;
+
+        System.out.println(doubleVariable);
+        System.out.println(doubleVariable1);
+    }
+}
