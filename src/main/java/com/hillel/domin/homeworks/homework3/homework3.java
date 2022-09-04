912e274d41fe31200baeebcb664cf857d8392485
Domin Artem <itomas612@gmail.com>
2022-09-05 00:02:21 +0300

    homework3

--- src/main/java/com/hillel/domin/homeworks/homework3/homework3.java
+++ src/main/java/com/hillel/domin/homeworks/homework3/homework3.java
@@ -1,5 +1,19 @@
 package com.hillel.domin.homeworks.homework3;
 
 public class homework3 {
-3233
+    public static void main(String[] args) {
+
+        int ab = 10;
+        int bd = 15;
+        int bc = 5;
+
+        int volume = ab*bd*bc;
+
+        System.out.println(volume);
+        System.out.println(ab*bd*bc + volume);
+
+        int length = 4*(ab+bd+bc);
+
+        System.out.println(length);
+    }
 }
