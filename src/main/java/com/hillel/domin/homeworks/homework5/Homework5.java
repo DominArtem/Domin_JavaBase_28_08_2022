d2ca0ba8a113abc81f3a51088bd240971f6a4818
Domin Artem <itomas612@gmail.com>
2022-09-16 20:29:21 +0300

    homework5

--- src/main/java/com/hillel/domin/homeworks/homework5/Homework5.java
+++ src/main/java/com/hillel/domin/homeworks/homework5/Homework5.java
@@ -4,22 +4,18 @@ import java.util.Scanner;
 
 public class Homework5 {
     public static void main(String[] args) {
-//
-//        int gam1Team1 = 0;
-//        int gam2Team1 = 0;
-//        int gam3Team1 = 0;
-//        int gam4Team1 = 0;
-//        int gam5Team1 = 0;
 
         Scanner scanner = new Scanner(System.in);
+
 
         System.out.println("enter team 1 name:");
         String team1 = scanner.nextLine();
         System.out.println(team1);
 
         System.out.println("frags gamer 1");
+        int gam1Team1 = 0;
         if (scanner.hasNextInt()) {
-            int gam1Team1 = scanner.nextInt();
+            gam1Team1 = scanner.nextInt();
             System.out.println(gam1Team1);
         } else {
             System.out.println("wrong data");
@@ -27,8 +23,9 @@ public class Homework5 {
         }
 
         System.out.println("frags gamer 2");
+        int gam2Team1 = 0;
         if (scanner.hasNextInt()) {
-            int gam2Team1 = scanner.nextInt();
+           gam2Team1 = scanner.nextInt();
             System.out.println(gam2Team1);
         } else {
             System.out.println("wrong data");
@@ -36,8 +33,9 @@ public class Homework5 {
         }
 
         System.out.println("frags gamer 3");
+        int gam3Team1 = 0;
         if (scanner.hasNextInt()) {
-            int gam3Team1 = scanner.nextInt();
+            gam3Team1 = scanner.nextInt();
             System.out.println(gam3Team1);
         } else {
             System.out.println("wrong data");
@@ -45,8 +43,9 @@ public class Homework5 {
         }
 
         System.out.println("frags gamer 4");
+        int gam4Team1 = 0;
         if (scanner.hasNextInt()) {
-            int gam4Team1 = scanner.nextInt();
+            gam4Team1 = scanner.nextInt();
             System.out.println(gam4Team1);
         } else {
             System.out.println("wrong data");
@@ -54,21 +53,26 @@ public class Homework5 {
         }
 
         System.out.println("frags gamer 5");
+        int gam5Team1 = 0;
         if (scanner.hasNextInt()) {
-            int gam5Team1 = scanner.nextInt();
+          gam5Team1 = scanner.nextInt();
             System.out.println(gam5Team1);
         } else {
             System.out.println("wrong data");
             System.exit(0);
-        }
-        int gam1Team1 = 0;
-        int gam2Team1 = 0;
-        int gam3Team1 = 0;
-        int gam4Team1 = 0;
-        int gam5Team1 = 0;
 
-        double sumTeam1 = ((gam1Team1 + gam2Team1 + gam3Team1 + gam4Team1 + gam5Team1) / 5);
-        System.out.println(sumTeam1);
+        }
+        System.out.println("Suma Frags team 1");
+        int sumFragT1 = 0;
+        System.out.println(sumFragT1=(gam5Team1 + gam4Team1 + gam3Team1 + gam2Team1 + gam1Team1)/5);
+
+        scanner.nextLine();
+
+
+
+
+
+
 
 
 
