e98a8828cdf0089bb5361c018ab5692f977a952a
Domin Artem <itomas612@gmail.com>
2022-09-16 21:24:25 +0300

    homework5final

--- src/main/java/com/hillel/domin/homeworks/homework5/Homework5.java
+++ src/main/java/com/hillel/domin/homeworks/homework5/Homework5.java
@@ -15,13 +15,13 @@ public class Homework5 {
 
         System.out.println("enter team 1 name:");
         String team1 = scanner.nextLine();
-        System.out.println(team1);
+//        System.out.println(team1);
 
         System.out.println("frags gamer 1");
 
         if (scanner.hasNextInt()) {
             gam1Team1 = scanner.nextInt();
-            System.out.println(gam1Team1);
+//            System.out.println(gam1Team1);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -30,8 +30,8 @@ public class Homework5 {
         System.out.println("frags gamer 2");
 
         if (scanner.hasNextInt()) {
-           gam2Team1 = scanner.nextInt();
-            System.out.println(gam2Team1);
+            gam2Team1 = scanner.nextInt();
+//            System.out.println(gam2Team1);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -41,7 +41,7 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam3Team1 = scanner.nextInt();
-            System.out.println(gam3Team1);
+//            System.out.println(gam3Team1);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -51,7 +51,7 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam4Team1 = scanner.nextInt();
-            System.out.println(gam4Team1);
+//            System.out.println(gam4Team1);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -60,16 +60,16 @@ public class Homework5 {
         System.out.println("frags gamer 5");
 
         if (scanner.hasNextInt()) {
-          gam5Team1 = scanner.nextInt();
-            System.out.println(gam5Team1);
+            gam5Team1 = scanner.nextInt();
+//            System.out.println(gam5Team1);
         } else {
             System.out.println("wrong data");
             System.exit(0);
 
         }
-        System.out.println("Suma Frags team 1");
+//        System.out.println("Suma Frags team 1");
         double sumFragT1 = (double) (gam5Team1 + gam4Team1 + gam3Team1 + gam2Team1 + gam1Team1) / 5;
-        System.out.println(sumFragT1);
+//        System.out.println(sumFragT1);
 
         scanner.nextLine();
 
@@ -87,7 +87,7 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam1Team2 = scanner.nextInt();
-            System.out.println(gam1Team2);
+//            System.out.println(gam1Team2);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -97,7 +97,7 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam2Team2 = scanner.nextInt();
-            System.out.println(gam2Team2);
+//            System.out.println(gam2Team2);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -107,7 +107,7 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam3Team2 = scanner.nextInt();
-            System.out.println(gam3Team2);
+//            System.out.println(gam3Team2);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -117,7 +117,7 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam4Team2 = scanner.nextInt();
-            System.out.println(gam4Team2);
+//            System.out.println(gam4Team2);
         } else {
             System.out.println("wrong data");
             System.exit(0);
@@ -127,45 +127,33 @@ public class Homework5 {
 
         if (scanner.hasNextInt()) {
             gam5Team2 = scanner.nextInt();
-            System.out.println(gam5Team2);
+//            System.out.println(gam5Team2);
         } else {
             System.out.println("wrong data");
             System.exit(0);
 
         }
-        System.out.println("Suma Frags team 2");
-        double sumFragT2 = (double) (gam5Team2 + gam4Team2 + gam3Team2 + gam2Team2 + gam1Team2)/5;
-        System.out.println(sumFragT2);
+//        System.out.println("Suma Frags team 2");
+        double sumFragT2 = (double) (gam5Team2 + gam4Team2 + gam3Team2 + gam2Team2 + gam1Team2) / 5;
+//        System.out.println(sumFragT2);
 
         scanner.nextLine();
 
-        if (sumFragT1 > sumFragT2){
-            System.out.println("Перемогла команда: " + team1 + "\n" + "набрала: " + (sumFragT1*5) + " очків" );
+        if (sumFragT1 > sumFragT2) {
+            System.out.println("Перемогла команда: " + team1 + "\n" + "набрала: " + (sumFragT1 * 5) + " очків");
         }
-        if (sumFragT1 < sumFragT2){
-            System.out.println("Перемогла команда: " + team2 + "\n" + "набрала: " + (sumFragT2*5) + " очків" );
+        if (sumFragT1 < sumFragT2) {
+            System.out.println("Перемогла команда: " + team2 + "\n" + "набрала: " + (sumFragT2 * 5) + " очків");
         }
-        if (sumFragT1 == sumFragT2){
+        if (sumFragT1 == sumFragT2) {
             System.out.println("Dead heat");
         }
 
         scanner.close();
 
 
-
-
-
-
-
-
-
-
     }
 
-
-//        System.out.println("enter team 2 name:");
-//        String team2 = scanner.nextLine();
-//        System.out.println(team2);
 
 
     }
