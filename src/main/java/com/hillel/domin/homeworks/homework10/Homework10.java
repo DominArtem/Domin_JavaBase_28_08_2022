c4720c8fb64f77d421fe331ee7042dacfec6f4f7
Domin Artem <itomas612@gmail.com>
2022-09-28 11:02:12 +0300

    Homework10

---
+++ src/main/java/com/hillel/domin/homeworks/homework10/Homework10.java
@@ -0,0 +1,17 @@
+package com.hillel.domin.homeworks.homework10;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Homework10 {
+    public static void main(String[] args) {
+
+        int [][] array = new int[10][10];
+
+    }
+
+
+
+
+}
