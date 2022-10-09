eed8dbbc9dab3207e5f8bb7f4011b3e48ecea3d9
Domin Artem <itomas612@gmail.com>
2022-10-09 10:44:19 +0300

    Homework6

--- src/main/java/com/hillel/domin/homeworks/homework6/Homework6.java
+++ src/main/java/com/hillel/domin/homeworks/homework6/Homework6.java
@@ -6,23 +6,19 @@ public class Homework6 {
 
     public static void main(String[] args) {
 
-//        Зробити гру, в якій потрібно відгадати ціле число, яке "загадав" комп'ютер у діапазоні від 0 до 10
-//        Користувач повинен мати можливість вгадувати в кілька спроб (мінімум 3).
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter any integer from 0 to 10");
 
         int num = 10;
         int no = ((int) (Math.random() * num));
-        int g = 0;
+
 
         for (int i = 3 ; i > 0 ; i--) {
-
-//
-        if (scanner.hasNextInt()) {
-        } else {
-        System.out.println("Wrong data");
-            System.exit(0);
+            if (scanner.hasNextInt()) {
+            } else {
+             System.out.println("Wrong data");
+             System.exit(0);
         }
 
 
@@ -30,7 +26,6 @@ public class Homework6 {
         int user = scanner.nextInt();
 
 
-//            System.out.println(no);
 
                 if (user == no) {
                     System.out.println("Exelent");
@@ -39,12 +34,11 @@ public class Homework6 {
                 } else if (user > no) {
                     System.out.println("The number is greater");
                     scanner.nextLine();
-//            break;
 
                 } else {
                     System.out.println("Number less");
                     scanner.nextLine();
-//            break;
+
 
 
                 }
